ee42e7478af503d3c88354f3845cf1644796c914
ahmed alhalfa <ah.alhalfa@gmail.com>
2024-10-27 20:41:40 +0300

    adjusted homescreen

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -69,10 +69,9 @@ function Home2() {
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
+            <h1>
               Feel free to <span className="purple">connect </span>with me
-            </p>
+            </h1>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
